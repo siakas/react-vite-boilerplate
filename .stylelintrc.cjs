17b62624309c068876b0aed8b969d2fced41cb40
Sanshiroh SAKAI <sansyo@gmail.com>
2022-11-12 16:28:07 +0900

    add tailwind css

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -7,5 +7,16 @@ module.exports = {
   rules: {
     // アルファベット順のソートを無効化（stylelint-config-recess-order のソートを優先）
     'order/properties-alphabetical-order': null,
+
+    // 不明な @rule に対するチェックのルール
+    // Tailwind CSS で利用する @rule を許可
+    'scss/at-rule-no-unknown': [
+      true,
+      { ignoreAtRules: ['tailwind', 'layer', 'apply'] },
+    ],
+
+    // 不明な関数に対するチェックのルール
+    // Tailwind CSS で利用する関数を許可
+    'function-no-unknown': [true, { ignoreFunctions: ['theme', 'screen'] }],
   },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    open: true,
+  },
   plugins: [react(), tsconfigPaths()],
 })
