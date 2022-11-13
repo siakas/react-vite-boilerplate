4bcf01c775121cd81e65e1ae0702a4cfb5f2420c
Sanshiroh SAKAI <sansyo@gmail.com>
2022-11-13 13:21:47 +0900

    add react query

--- src/components/ClassicalFetchA.tsx
+++ src/components/ClassicalFetchA.tsx
@@ -12,17 +12,17 @@ export const ClassicalFetchA: FC = () => {
   const { isLoading, isError, tasks } = useClassicalFetch()
   console.log('rendered ClassicalFetchA')
 
+  // ローディング時とエラー時に返す画面をシンプルに変更
+  if (isLoading) return <div>Loading...</div>
+  if (isError) return <div>Error</div>
+
   return (
     <div className="flex justify-center items-center flex-col">
       <p className="text-center font-bold mb-3">ClassicalFetchA</p>
 
-      {isError && <div>Error</div>}
-
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        tasks?.map((task) => <p key={task.id}>{task.title}</p>)
-      )}
+      {tasks?.map((task) => (
+        <p key={task.id}>{task.title}</p>
+      ))}
 
       <p className="my-4 leading-7">
         本コンポーネント（ClassicalFetchA）がマウント（表示）されるたびに REST
